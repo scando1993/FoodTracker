df06fab9615c11d6f5624fe3cc5729f7e43a2378
root <ascacere92@gmail.com>
2016-08-18 22:58:49 -0500

    added main eskeleton, added customized actionbar activity and main activity with gridlayout.

--- app/src/main/java/com/development/whitefox/foodtracker/MainLayoutActivity.java
+++ app/src/main/java/com/development/whitefox/foodtracker/MainLayoutActivity.java
@@ -1,13 +1,18 @@
 package com.development.whitefox.foodtracker;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainLayoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.principal);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
